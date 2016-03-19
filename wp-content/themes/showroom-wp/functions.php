59661ceaa6710663865f744cbcda5992a53c1d68
Francisco Mat <francisco@f5sites.com>
2016-03-19 15:18:55 -0300

    bootstrap responsive wp menu with collapse

--- wp-content/themes/showroom-wp/functions.php
+++ wp-content/themes/showroom-wp/functions.php
@@ -8,7 +8,9 @@ add_action("admin_init", "display_theme_panel_fields");
 function enqueue() {
 	//styles
 	wp_enqueue_style("bootstrap-css", get_template_directory_uri()."/css/bootstrap.min.css");
+	wp_enqueue_style("bootstrap-css", get_template_directory_uri()."/css/normalize.css");
 	wp_enqueue_style("style-css", get_template_directory_uri()."/style.css");
+
 	//scripts
 	wp_enqueue_script("jquery");
 	wp_enqueue_script("showroom-js", get_template_directory_uri()."/js/bootstrap.min.js");
--- wp-content/themes/showroom-wp/header.php
+++ wp-content/themes/showroom-wp/header.php
@@ -9,21 +9,51 @@
 <body <?php body_class(); ?>>
 
 
+<div id="header-advt" class="row text-center">
+	<img src="<?php echo bloginfo('stylesheet_directory') ?>/imgs/topo-ad.png" alt="Banner">
+</div>
 
 <div class="container">
-	<header id="masthead" class="site-header" role="banner">
-		<div id="header-advt" class="row text-center">
-			<img src="<?php echo bloginfo('stylesheet_directory') ?>/imgs/topo-ad.png" alt="Banner">
-		</div>
-		<div id="header-top" class="row">
-			<img src="<?php echo bloginfo('stylesheet_directory') ?>/imgs/logo-showroom.png" alt="ShowRoom">
-			<nav id="primary-navigation" role="navigation">
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'menu_id' => 'primary-menu' ) ); ?>
-			</nav>
-		</div>
-		<div id="header-extra" class="row">
-			+ ACESSADOS
-		</div>
+	<header id="masthead" class="site-header row" role="banner">
+		
+			<div class="col-sm-2">
+				<img src="<?php echo bloginfo('stylesheet_directory') ?>/imgs/logo-showroom.png" alt="ShowRoom">
+			</div>
+
+			<div class="col-sm-8">
+				<nav id="primary-navigation" role="navigation">
+					
+					 <nav class="navbar ">
+					  <div class="container-fluid">
+					    <div class="navbar-header">
+					      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        A
+					      </button>
+					    </div>
+					    <div class="collapse navbar-collapse" id="myNavbar">
+					    	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav navbar-nav', 'menu_id' => 'primary-menu', "container" => "ul" ) ); ?>
+					      <!--ul class="nav navbar-nav">
+					        <li class="active"><a href="#">Home</a></li>
+					        <li><a href="#1">Page 1</a></li>
+					        <li><a href="#2">Page 2</a></li>
+					        <li><a href="#">Page 3</a></li>
+					      </ul-->
+					    </div>
+					  </div>
+					</nav>
+				</nav>
+			</div>
+
+			<div class="col-sm-2">
+				<?php get_search_form(); ?>
+			</div>
+		
 	</header><!-- #masthead -->
 
+	<div id="header-extra" class="row">
+		+ ACESSADOS
+	</div>
 	<div id="main" class="site-main">
