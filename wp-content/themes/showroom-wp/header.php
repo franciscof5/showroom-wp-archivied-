3f0c5b70b30852c86a12a566cb96ef164f524672
Francisco Mat <francisco@f5sites.com>
2016-03-19 01:48:24 -0300

    basic templates files

---
+++ wp-content/themes/showroom-wp/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head <?php language_attributes(); ?> >
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<title>ShowRoom WP</title>
+	<?php wp_head(); 
+	//
+	/*wp_enqueue_style("bootstrap-css", get_template_directory_uri()."/css/bootstrap.min.css" );
+	//
+	wp_enqueue_script("jquery2-js", get_template_directory_uri()."/js/jquery2.min.js");
+	wp_enqueue_script("bootbox-js", get_template_directory_uri()."/js/bootbox.min.js");
+	wp_enqueue_script("bootstrap-js", get_template_directory_uri()."/js/bootstrap.min.js");*/
+	?>
+	
+</head>
+<body <?php body_class(); ?>>
+
+<div class="container">
---
+++ wp-content/themes/showroom-wp/index.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+		
+<h4>SHOWROOM-WP</h4>
+
+<?php get_footer(); ?>
+  
