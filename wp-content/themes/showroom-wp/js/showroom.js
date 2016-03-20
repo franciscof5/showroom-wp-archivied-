4c054f332bd397be84b58d2ce5f11b9962f8674f
Francisco Mat <francisco@f5sites.com>
2016-03-19 22:09:29 -0300

    menu click bug^

--- wp-content/themes/showroom-wp/js/showroom.js
+++ wp-content/themes/showroom-wp/js/showroom.js
@@ -2,10 +2,8 @@ jQuery(document).ready(function($) {
 	$("ul.children").addClass('dropdown-menu');
 	$("ul.children").parent().wrap( "<li class='dropdown'></li>" );
 	$("ul.children").parent().unwrap();
-	$("ul.children").parent().find("a").attr("data-toggle","dropdown");
-	$("ul.children").parent().find("a").addClass("dadropdownta-toggle","dropdown");
+	$("ul.children").prev().attr("data-toggle","dropdown");
+	$("ul.children").prev().addClass("dadropdownta-toggle","dropdown");
 	$("ul.children").prev().append('<span class="caret" style="float:right;margin-top:8px;"></span>');
 	$("dadropdownta-toggle").unwrap();
-	
-	//$('<span class="caret"></span>').insertBefore("ul.children");
 });
