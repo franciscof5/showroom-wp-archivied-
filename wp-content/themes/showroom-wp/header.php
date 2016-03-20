ff752f02cdaadff35dadaf204b97031c513e949e
Francisco Mat <francisco@f5sites.com>
2016-03-19 22:07:42 -0300

    menu refinments, font install

--- wp-content/themes/showroom-wp/header.php
+++ wp-content/themes/showroom-wp/header.php
@@ -13,31 +13,35 @@
 	<img src="<?php echo bloginfo('stylesheet_directory') ?>/imgs/topo-ad.png" alt="Banner">
 </div>
 
-<div class="container">
+<div class="container-fluid">
 	<header id="masthead" class="site-header row" role="banner">
 		
 
 			
 				<nav id="primary-navigation" role="navigation">
 					<div class="navbar-header text-center">
-				      <img src="<?php echo bloginfo('stylesheet_directory') ?>/imgs/logo-showroom.png" alt="ShowRoom">
+				      <img src="<?php echo bloginfo('stylesheet_directory') ?>/imgs/logo-showroom.png" alt="ShowRoom" id="header-logo">
 				      <div class="navbar-rightss" style="float:right;">
 					      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-					        A
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
 					      </button>
 					    </div>
 				    </div>
 					 
 					  <div class="container-fluid">
-					    
-
 					    <div class="collapse navbar-collapse  navbar-right" id="myNavbar">
 					    	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav navbar-nav', 'menu_id' => 'primary-menu', "container" => "ul" ) ); ?>
 					      	<div class="navbar-right">
-					    		<?php get_search_form(); ?>
+					    		<form role="search" method="get" id="searchform" action="http://www.example.com/" >
+									<input type="text" placeholder="PESQUISAR" value="" name="s" id="s" />
+									<!--input type="submit" id="searchsubmit" value="Search" -->
+									<!--img src="<?php echo bloginfo('stylesheet_directory') ?>/imgs/logo-showroom.png" alt="ShowRoom"-->
+									</div>
+								</form>
 							</div>
 					    </div>
-					    
 					  </div>
 					
 				</nav>
